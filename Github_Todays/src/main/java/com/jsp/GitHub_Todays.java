75cbe821434830dc22f33741efbdac9e2ddcbade
prakashiOrbit <malooprakashh@gmail.com>
2022-10-25 11:09:41 +0530

    this is commit

---
+++ Github_Todays/src/main/java/com/jsp/GitHub_Todays.java
@@ -0,0 +1,10 @@
+package com.jsp;
+
+public class GitHub_Todays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi this is latest commit");
+	}
+
+}
